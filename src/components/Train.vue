afba0cada18abc06ca40758b1a0944e9aa28de9c
Rilong <romgnatyuk@gmail.com>
2018-05-29 15:42:25 +0300

    train finish actions

--- src/components/Train.vue
+++ src/components/Train.vue
@@ -4,29 +4,23 @@
       <v-btn color="success" large v-if="!isStart" @click="start">Start</v-btn>
       <v-btn color="warning" large v-else @click="restart">Restart</v-btn>
     </v-layout>
-    <v-layout>
+    <v-layout v-if="isStart">
       <v-flex xs6 offset-xs3 column>
         <v-card>
           <v-card-title>
-            <h2 class="text-xs-center" style="width: 100%">Acquaintance</h2>
+            <h2 class="text-xs-center" style="width: 100%">{{mainWord.word}}</h2>
           </v-card-title>
           <v-divider></v-divider>
           <v-card-text>
-            <div>
-              <v-btn class="train-btn" round>Знайомий</v-btn>
-            </div>
-            <div>
-              <v-btn class="train-btn" round>Стати</v-btn>
-            </div>
-            <div>
-              <v-btn class="train-btn" round>Коштує</v-btn>
+            <div v-for="activeWord in activeWords">
+              <v-btn class="train-btn" round @click="onActive(activeWord.word_id)" :class="{primary: activeWord.active}">{{activeWord.word_translation}}</v-btn>
             </div>
           </v-card-text>
           <v-card-actions class="pb-4">
-            <span>1 of 70</span>
+            <span>{{wordCount}} of {{words.length}}</span>
             <v-spacer></v-spacer>
-            <v-btn color="error">I do not remember</v-btn>
-            <v-btn color="success">OK</v-btn>
+            <v-btn color="error" @click="onRemember">I don't remember</v-btn>
+            <v-btn color="success" @click="onOk">OK</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -38,15 +32,43 @@
   export default {
     data () {
       return {
+        wordCount: 1,
         isStart: false
       }
     },
-    computed: {},
+    computed: {
+      words () {
+        return this.$store.getters.words
+      },
+      mainWord () {
+        return this.$store.getters.mainTrainWord
+      },
+      activeWords () {
+        return this.$store.getters.trainWordsActive
+      }
+    },
     methods: {
       start () {
         this.isStart = true
+        this.$store.dispatch('trainStart')
       },
-      restart () {}
+      restart () {
+        this.$store.dispatch('trainRestart')
+        this.wordCount = 1
+      },
+      onActive (id) {
+        this.$store.commit('setTrainSelectedWord', id)
+      },
+      onRemember () {
+
+      },
+      onOk () {
+        this.$store.dispatch('trainWordCheck')
+          .then(() => {
+            this.wordCount++
+          })
+          .catch(() => {})
+      }
     }
   }
 </script>
--- src/store/train.js
+++ src/store/train.js
@@ -1,11 +1,18 @@
+import _ from 'lodash'
+
 export default {
   state: {
+    mainTrainWord: null,
     trainWords: null,
     trainWordsGood: null,
     trainWordsMistakes: 0,
-    trainWordsActive: null
+    trainWordsActive: null,
+    isReverse: false
   },
   mutations: {
+    setMainWord (state, payload) {
+      state.mainTrainWord = payload
+    },
     setTrainWords (state, payload) {
       state.trainWords = payload
     },
@@ -17,10 +24,62 @@ export default {
     },
     setTrainWordsActive (state, payload) {
       state.trainWordsActive = payload
+    },
+    setTrainSelectedWord (state, payload) {
+      state.trainWordsActive = _.map(state.trainWordsActive, o => {
+        o['active'] = o.word_id === payload
+        return o
+      })
     }
   },
-  actions: {},
+  actions: {
+    trainStart ({commit, getters}) {
+      let words = getters.trainWords
+      if (words === null) {
+        commit('setTrainWords', _.clone(getters.words))
+        words = _.clone(getters.words)
+      }
+
+      let mainWord = _.sample(words)
+      let wordsWithout = _.clone(getters.words)
+
+      _.remove(wordsWithout, o => o.word_id === mainWord.word_id)
+      let activeWords = _.sampleSize(wordsWithout, 2)
+
+      activeWords.push(mainWord)
+      activeWords = _.shuffle(_.map(activeWords, o => {
+        o['active'] = false
+        return o
+      }))
+      commit('setMainWord', mainWord)
+      commit('setTrainWordsActive', activeWords)
+    },
+    trainRestart ({commit, getters, dispatch}) {
+      commit('setTrainWords', null)
+      dispatch('trainStart')
+    },
+    trainWordCheck ({commit, getters, dispatch}) {
+      let selected = getters.selected
+      let mainWord = getters.mainTrainWord
+      let words = getters.trainWords
+      if (typeof selected !== 'undefined') {
+        return new Promise((resolve, reject) => {
+          if (selected.word === mainWord.word) {
+            _.remove(words, o => o.word_id === mainWord.word_id)
+            commit('setTrainWords', words)
+            dispatch('trainStart')
+            resolve()
+          } else {
+            reject(new Error('Mistake'))
+          }
+        })
+      }
+    }
+  },
   getters: {
+    mainTrainWord (state) {
+      return state.mainTrainWord
+    },
     trainWords (state) {
       return state.trainWords
     },
@@ -32,6 +91,9 @@ export default {
     },
     trainWordsActive (state) {
       return state.trainWordsActive
+    },
+    selected (state) {
+      return _.find(state.trainWordsActive, {active: true})
     }
   }
 }
